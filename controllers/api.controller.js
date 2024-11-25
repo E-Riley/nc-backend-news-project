618e3013ad40f7016e20ef0347d862c72ebe3e00
ethan <ethanjriley02@gmail.com>
2024-11-25 14:14:04 +0000

    Removes console logs

--- controllers/api.controller.js
+++ controllers/api.controller.js
@@ -15,7 +15,6 @@ exports.getTopics = (req, res, next) => {
 
 exports.getArticleById = (req, res, next) => {
   const articleId = req.params.article_id;
-  console.log("Inside controlle with id " + articleId);
   selectArticleById(articleId)
     .then((article) => {
       res.status(200).send({ article });
