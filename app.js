6d0d57d9d98a351969765c135443a180c8935419
ethan <ethanjriley02@gmail.com>
2024-11-25 12:01:45 +0000

    Adds GET /api/topics endpoint, and relevant error handling

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const express = require("express");
-const { getEndpoints } = require("./controllers/api.controller");
+const { getEndpoints, getTopics } = require("./controllers/api.controller");
 
 const app = express();
 
 app.get("/api", getEndpoints);
+
+app.get("/api/topics", getTopics);
+
+app.all("*", (req, res, next) => {
+  res.status(400).send({ msg: "Bad request" });
+  next();
+});
+
+app.use((err, req, res, next) => {
+  if (err.status && err.msg) {
+    res.status(err.status).send({ msg: err.msg });
+  } else {
+    next(err);
+  }
+});
 
 module.exports = app;
--- controllers/api.controller.js
+++ controllers/api.controller.js
@@ -1,5 +1,14 @@
 const endpoints = require("../endpoints.json");
+const { selectTopics } = require("../models/api.models");
 
 exports.getEndpoints = (req, res) => {
   res.status(200).send({ endpoints });
+};
+
+exports.getTopics = (req, res, next) => {
+  selectTopics()
+    .then((topics) => {
+      res.status(200).send({ topics });
+    })
+    .catch(next);
 };
