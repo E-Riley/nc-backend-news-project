f4ef6759d598e5c3e9505fd1e5d838330cc4393d
ethan <ethanjriley02@gmail.com>
2024-11-27 09:54:40 +0000

    Adds sorting queries to GET /api/articles endpoint

--- models/articles.models.js
+++ models/articles.models.js
@@ -15,12 +15,33 @@ exports.selectArticleById = (article_id) => {
     });
 };
 
-exports.selectArticles = () => {
+exports.selectArticles = (sort_by = "created_at", order = "desc") => {
+  const validSortBy = [
+    "article_id",
+    "title",
+    "topic",
+    "author",
+    "body",
+    "created_at",
+    "votes",
+  ];
+
+  const validOrder = ["ASC", "DESC"];
+
+  if (
+    !validSortBy.includes(sort_by) ||
+    !validOrder.includes(order.toUpperCase())
+  ) {
+    return Promise.reject({
+      status: 400,
+      msg: "Bad request",
+    });
+  }
   return db
     .query(
       `SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, 
       articles.article_img_url, CAST(COUNT(comments.article_id) as INT) AS comment_count FROM articles LEFT OUTER JOIN comments ON 
-      articles.article_id = comments.article_id GROUP BY articles.article_id ORDER BY articles.created_at DESC`
+      articles.article_id = comments.article_id GROUP BY articles.article_id ORDER BY articles.${sort_by} ${order}`
     )
     .then(({ rows }) => {
       return rows;
