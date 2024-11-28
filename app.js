e4dc899a976aa9451b2bc77416fe142a65069a6e
ethan <ethanjriley02@gmail.com>
2024-11-28 16:28:24 +0000

    Refactors endpoints to use express routers

--- app.js
+++ app.js
@@ -1,37 +1,10 @@
 const express = require("express");
-const { getEndpoints, getTopics } = require("./controllers/api.controller");
-const {
-  getArticleById,
-  getArticles,
-  patchArticle,
-} = require("./controllers/articles.controller");
-const {
-  getCommentsByArticle,
-  postCommentToArticle,
-  deleteComment,
-} = require("./controllers/comments.controller");
-const { getUsers } = require("./controllers/users.controller");
+const apiRouter = require("./routes/api.router");
 
 const app = express();
 app.use(express.json());
 
-app.get("/api", getEndpoints);
-
-app.get("/api/topics", getTopics);
-
-app.get("/api/articles/:article_id", getArticleById);
-
-app.get("/api/articles", getArticles);
-
-app.get("/api/articles/:article_id/comments", getCommentsByArticle);
-
-app.post("/api/articles/:article_id/comments", postCommentToArticle);
-
-app.patch("/api/articles/:article_id", patchArticle);
-
-app.delete("/api/comments/:comment_id", deleteComment);
-
-app.get("/api/users", getUsers);
+app.use("/api", apiRouter);
 
 app.all("*", (req, res, next) => {
   res.status(404).send({ msg: "Endpoint not found" });
